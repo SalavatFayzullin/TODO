abaaf45309c540b42cdfead5e1d898d238a7779b
SalavatFayzullin <salikf2005@gmail.com>
2025-03-13 11:34:54 +0300

    Final commit

---
+++ src/main/java/ru/salavat/todo_application/controller/AuthController.java
@@ -0,0 +1,18 @@
+package ru.salavat.todo_application.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthController {
+
+    @GetMapping("/login")
+    public String showLoginPage(@RequestParam(value = "error", required = false) String error, Model model) {
+        if (error != null) {
+            model.addAttribute("errorMessage", "Неверные учетные данные");
+        }
+        return "login"; // Имя шаблона (например, login.html)
+    }
+}
--- src/main/java/ru/salavat/todo_application/security/SecurityConfig.java
+++ src/main/java/ru/salavat/todo_application/security/SecurityConfig.java
@@ -20,12 +20,20 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests(httpRequests ->  httpRequests
-                .requestMatchers("/register**").permitAll()
+                .requestMatchers("/register**", "/login").permitAll()
                 .anyRequest().authenticated())
                 .formLogin(formLogin -> formLogin
+                        .loginPage("/login") // Укажите кастомную страницу логина
+                        .loginProcessingUrl("/perform_login") // URL для обработки формы
+                        .usernameParameter("username") // Параметр имени пользователя
+                        .passwordParameter("password") // Параметр пароля
+                        .failureUrl("/login?error=true") // Перенаправление при ошибке
                         .defaultSuccessUrl("/tasks", true))
                 .userDetailsService(userDetailsService)
                 .csrf(AbstractHttpConfigurer::disable)
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login"))
                 .build();
     }
 }
